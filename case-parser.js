1dc62aa27919ea0eebfb9119f8fb45eb80b0c4a7
Ruslan Rybak <henergarden@gmail.com>
2024-04-19 00:29:29 +0300

    Added new parseGroup and finalObjectCreation functions
    + changed git ignoring

--- case-parser.js
+++ case-parser.js
@@ -3,33 +3,17 @@ const axios = require('axios');
 const sheetId = '15s7tQOnTBGD6Bfr08IjQU8pXGM0WtH6tYoHpvuP28o8';
 const base = `https://docs.google.com/spreadsheets/d/${sheetId}/gviz/tq?`;
 const query = encodeURIComponent('Select *');
+const caseName = 'usual_basic_case';
 
 async function fetchData() {
   try {
   let currencyList = await parseTable('843450455','A3:D9');
-  // console.log('Список валют:');
-  // console.log(JSON.stringify(currencyList, null, 2));
-
   let itemsList = await parseTable('843450455','F3:I105');
-  // console.log('Список предметов:');
-  // console.log(JSON.stringify(itemsList, null, 2));
-  
-
-
+  let fullList = currencyList.concat(itemsList);
   let group_1 = await parseTable('0','D7:F14');
-  // console.log('Группа 1:');
-  // console.log(JSON.stringify(group_1, null, 2));
-
-
-
   let group_2 = await parseTable('0','H7:J11');
-  // console.log('Группа 2:');
-  // console.log(JSON.stringify(group_2, null, 2));
-
-
   let group_3 = await parseTable('0','L7:N12');
-  // console.log('Группа 3:');
-  // console.log(JSON.stringify(group_3, null, 2));
+  await finalObjectCreation(caseName,group_1,group_2,group_3,fullList);
      
   } catch (error) {
       console.error('Error fetching data:', error);
@@ -40,7 +24,6 @@ fetchData();
 
 async function parseTable(gid, sheetRange) {
   const url = `${base}&gid=${gid}&range=${sheetRange}&tq=${query}`;
-  console.log(url);
   const response = await axios.get(url);
   const rep = response.data;
   const jsData = JSON.parse(rep.substring(47).slice(0,-2));
@@ -63,4 +46,53 @@ async function parseTable(gid, sheetRange) {
   });
   
   return data;
+}
+
+function parseGroup(groupId, groupName, groupChance, fullList){
+    let groupFinalObject = {};
+    groupFinalObject[groupName] = {
+        group_chance: groupChance,
+        rewards: []
+    }
+
+    groupId.forEach((object) => {
+        let itemObject = {};
+        let newItemObject = {};
+
+        fullList.forEach(el => {
+            if (el['Название'] == object['Награда']) {
+                itemObject[object['Награда']] = {};
+                itemObject[object['Награда']][`type`] = el.type;
+                itemObject[object['Награда']][`parameters`] = {
+                    'count': object['count'],
+                    'item_id': el.item_id,
+                    'chance': object['chance'],
+                    };
+            newItemObject[el['Нейминг']] = itemObject[object['Награда']];
+            groupFinalObject[groupName].rewards.push(newItemObject);
+            
+            }
+
+        })
+        
+    })
+
+    return groupFinalObject;
+}
+
+
+async function finalObjectCreation(caseName,group_1,group_2,group_3,fullList){
+    let finalObject = {};
+    finalObject[caseName] = {
+            groups: []
+    }
+    
+    finalObject[caseName].groups.push(parseGroup(group_1,'group_1',0.25,fullList));
+    finalObject[caseName].groups.push(parseGroup(group_2,'group_2',0.15,fullList));
+    finalObject[caseName].groups.push(parseGroup(group_3,'group_3',0.6,fullList));
+    
+    const fs = require('fs');
+    fs.writeFileSync('case.json', (JSON.stringify(finalObject, null, 2)), {encoding: 'utf-8', flag: 'w'});
+    console.log('case.json has been created');
+
 }
